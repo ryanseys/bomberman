d078f88313b5d43e79fb630a70644a8372374413
Fady Ibrahim <fady31@hotmail.com>
2014-03-01 05:54:00 -0500

    client sender's run has been implemented.

--- code/Client/src/ClientReceiver.java
+++ code/Client/src/ClientReceiver.java
@@ -1,14 +1,25 @@
+import java.net.DatagramSocket;
+
 
 public class ClientReceiver extends Thread {
-
-	public ClientReceiver() {
+	private MessageQueue recMsgQ;
+	private ClientView cView;
+	public ClientReceiver(MessageQueue recMsgQ,ClientView cView) {
 		// TODO Auto-generated constructor stub
+		this.cView=cView;
+		this.recMsgQ=recMsgQ;
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		super.run();
-	}
+		synchronized(recMsgQ)
+		{
+			while(recMsgQ.isEmpty())
+			{
+				
+			}
+		}
+	}	
 
 }
