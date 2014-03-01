c10f6f7ff8554cc90ca41df7b04dd623bd8f4c83
Martin Gingras <martin.p.gingras@gmail.com>
2014-02-28 23:11:03 -0500

    Added basic messaging to Client

--- code/Client/src/Client.java
+++ code/Client/src/Client.java
@@ -1,9 +1,37 @@
-
+import java.io.*;
+import java.net.*;
 public class Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		final int SERVER_PORT = 3000;
+		
+		/*** TEMP READER FOR TESTING ***/
+		BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+		DatagramSocket clientSocket = new DatagramSocket();
+		// For now localhost, prompt later for external host
+		InetAddress IPAddress = InetAddress.getByName("localhost");
+		byte[] sendData = new byte[1024];
+		byte[] receiveData = new byte[1024];
+		
+		while(true){
+			String msg = userInput.readLine();
+			sendData = msg.getBytes();
+			// Send message to the Server
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, SERVER_PORT);
+			clientSocket.send(sendPacket);
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			
+			// Get message from the Server
+			clientSocket.receive(receivePacket);
+			String serverMsg = new String(receivePacket.getData());
+			System.out.println("FROM SERVER:" + serverMsg);
 
+		}
+		
+		//clientSocket.close();
 	}
 
 }
