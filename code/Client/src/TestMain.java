a3573c8fef41c41353ae81e3a25783796dce8b3a
Martin Gingras <martin.p.gingras@gmail.com>
2014-03-01 16:52:38 -0500

    try/catch

--- code/Client/src/TestMain.java
+++ code/Client/src/TestMain.java
@@ -5,7 +5,12 @@ public class TestMain {
 	public static void main(String args[]) {
 		System.out.println("Running tests...");
 		TestClient tc = new TestClient(server, port);
-		tc.runAllTests();
+		try {
+			tc.runAllTests();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("Finished running tests!");
 	}
 }
