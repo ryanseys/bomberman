25b2f9d383ddbd6cfde7fdd7b40f17a0bc11f6f8
Martin Gingras <martin.p.gingras@gmail.com>
2014-02-28 20:28:10 -0500

    Client stores information about our clients so that we can broadcast the game state

---
+++ code/Server/src/Client.java
@@ -0,0 +1,56 @@
+import java.net.*;
+
+public class Client {
+	private InetAddress IPaddr;
+	private int port;
+	private int id;
+	
+	public Client(InetAddress IP, int port, int id) {
+		this.IPaddr = IP;
+		this.port = port;
+		this.id = id;
+	}
+
+	/**
+	 * @return the iPaddr
+	 */
+	public InetAddress getIPaddr() {
+		return IPaddr;
+	}
+
+	/**
+	 * @param iPaddr the iPaddr to set
+	 */
+	public void setIPaddr(InetAddress iPaddr) {
+		IPaddr = iPaddr;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * @param port the port to set
+	 */
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
