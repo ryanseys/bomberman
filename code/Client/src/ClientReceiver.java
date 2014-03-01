867d5a7a8c49fdbbf612e554fcc47a4773a6a792
Fady Ibrahim <fady31@hotmail.com>
2014-03-01 06:33:11 -0500

    client reciever's run has been implemented.

--- code/Client/src/ClientReceiver.java
+++ code/Client/src/ClientReceiver.java
@@ -1,25 +1,36 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 
 public class ClientReceiver extends Thread {
+	private static final int size = 1000;
 	private MessageQueue recMsgQ;
-	private ClientView cView;
-	public ClientReceiver(MessageQueue recMsgQ,ClientView cView) {
+	private DatagramSocket dsocket;
+	private DatagramPacket data;
+	public ClientReceiver(MessageQueue recMsgQ,DatagramSocket dsocket) {
 		// TODO Auto-generated constructor stub
-		this.cView=cView;
 		this.recMsgQ=recMsgQ;
+		this.dsocket=dsocket;
 	}
 
 	@Override
 	public void run() {
+		byte strdata[]=new byte[size];
 		// TODO Auto-generated method stub
+		data= new DatagramPacket(strdata,size);
+		try {
+			dsocket.receive(data);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		synchronized(recMsgQ)
 		{
-			while(recMsgQ.isEmpty())
-			{
-				
-			}
+			recMsgQ.add(new String((data.getData())));
 		}
+		recMsgQ.notify();	
 	}	
 
 }
