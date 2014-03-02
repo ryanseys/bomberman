663cfc571ffed2b575d37283bfb80a184c29d3bf
Martin Gingras <martin.p.gingras@gmail.com>
2014-03-02 15:04:35 -0500

    Added functionality to serverSender, no longer threaded since it makes non-blocking calls.

--- code/Server/src/ServerSender.java
+++ code/Server/src/ServerSender.java
@@ -1,21 +1,28 @@
-import java.net.DatagramSocket;
+import java.io.IOException;
+import java.net.*;
 
 
-public class ServerSender extends Thread {
+public class ServerSender {
 	private DatagramSocket serverSocket;
 	public ServerSender(DatagramSocket serverSocket) {
 		this.serverSocket = serverSocket;
+	}	
+	public void sendClientMsg(Client client, String message){
+		sendMsg(message, client.getIPaddr(), client.getPort());
 	}
-	@Override
-	public void run() {
-		
-		super.run();
+	
+	public void broadcastMessage(Client[] clients, String message){	
+		for (Client client : clients) {
+			sendClientMsg(client, message);
+		}
 	}
-	public void sendClientMsg(Client client, String msg){
-		
+	public void sendMsg(String message, InetAddress IP, int port){
+		byte[] msg = message.getBytes();
+		 DatagramPacket packet = new DatagramPacket(msg, msg.length, IP, port);
+		try {
+			serverSocket.send(packet);
+		} catch (IOException e) {
+			System.out.println("Error broadcasting message");
+		}
 	}
-	public void braodcastGameState(Game game){
-		
-	}
-
 }
