3590f3473e5c3db85e6388760919e6e01107dab3
Martin Gingras <martin.p.gingras@gmail.com>
2014-03-21 11:57:10 -0400

    basic node shit

--- code/client_node/app.js
+++ code/client_node/app.js
@@ -8,8 +8,10 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-var ws = require('ws');
 var dgram = require('dgram'); // http://nodejs.org/api/dgram.html
+
+var ws = require('ws');
+var WebSocketServer = ws.Server;
 
 var app = express();
 
@@ -33,6 +35,62 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+var wss = new WebSocketServer({
+  server: server
+});
+
+
+// WebSocket Server Handlers!!!
+client_id = 0;
+wss.on('connection', function(ws) {
+  var id;
+  id = client_id++;
+  console.log("Client Connect[" + new Date() + "]: " + id);
+
+
+
+  // Define ws handlers here...
+  return ws.on('close', function() {
+    return console.log("Client Disconnect[" + new Date() + "]: " + id);
+  });
+  return ws.on('up', function() {
+    return console.log("UP[" + new Date() + "]: " + id);
+  });
+  return ws.on('down', function() {
+    return console.log("DOWN[" + new Date() + "]: " + id);
+  });
+  return ws.on('left', function() {
+    return console.log("LEFT[" + new Date() + "]: " + id);
+  });
+  return ws.on('right', function() {
+    return console.log("RIGHT[" + new Date() + "]: " + id);
+  });
+  return ws.on('deploy', function() {
+    return console.log("DEPLOY[" + new Date() + "]: " + id);
+  });
+  return ws.on('start', function() {
+    return console.log("START[" + new Date() + "]: " + id);
+  });
+  return ws.on('end', function() {
+    return console.log("END[" + new Date() + "]: " + id);
+  });
+
+});
+
+// Broadcast message to all clients
+wss.broadcast = function(data) {
+  var i, _results;
+  _results = [];
+  for (i in this.clients) {
+    _results.push(this.clients[i].send(data, function(err) {
+      if (err) {
+        return console.log(err);
+      }
+    }));
+  }
+  return _results;
+};
